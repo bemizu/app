dd5215d0c2b21aca75b79aae5e2f825cdf6c67d5
Sam <samullman@gmail.com>
2021-02-09 21:53:47 -0800

    better spacing

--- components/Home/Logged.js
+++ components/Home/Logged.js
@@ -5,6 +5,7 @@ import Section from "../section";
 import {
     Box,
     Heading,
+    SimpleGrid,
 } from "@chakra-ui/react";
   
 
@@ -13,11 +14,26 @@ function Logged ({ session }) {
         <Layout title={ session.user.username }>
         <Section>
           <Container maxWidth="1200px">
+              <SimpleGrid columns={[1, 2, 3]} spacing={[4, 8, 12]}>
+              <Box bg="white" p={[3, 6]} rounded="lg" shadow="lg">
+              <Heading>
+                Welcome
+              </Heading>
+            </Box>
+
             <Box bg="white" p={[3, 6]} rounded="lg" shadow="lg">
               <Heading>
                 Welcome
               </Heading>
             </Box>
+
+            <Box bg="white" p={[3, 6]} rounded="lg" shadow="lg">
+              <Heading>
+                Welcome
+              </Heading>
+            </Box>
+              </SimpleGrid>
+            
           </Container>
         </Section>
 
