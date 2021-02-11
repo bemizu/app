814021e882968e3500f03446d33a4509d3fadb28
Sam <samullman@gmail.com>
2021-02-11 15:22:43 -0800

    update page

--- pages/profile/[username].js
+++ pages/profile/[username].js
@@ -7,7 +7,7 @@ import Session from "../../contexts/session";
 
 function Page() {
   const session = Session((state) => state);
-  const user = session.user;
+  const user = session.user || {};
   const profile = user.profile;
 
   let fullName = profile.fullName ? <Heading size="sm" mb={2} color="gray.700">{ profile.fullName }</Heading> : "";
