4739631ba8b4764d32cb3a37d1a23aa71657f26d
Sam <samullman@gmail.com>
2021-03-25 14:13:16 -0700

    redirect url

--- pages/settings.js
+++ pages/settings.js
@@ -1,56 +1,54 @@
 import {
-    Box,
-    ButtonGroup,
-    Heading,
-    SimpleGrid,
-    Button,
-    Grid,
-    Divider,
-    Container,
-  } from "@chakra-ui/react";
-  import Loading from "../components/Home/Loading";
-  import Layout from "../components/layout";
-  import PageContainer from "../components/pageContainer";
-  import styled from "@emotion/styled";
-  import theme from "../public/theme";
-  import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
-  import Session from "../contexts/session";
-  import { useRouter, withRouter } from "next/router";
-  import { useEffect, useState } from "react";
-  
-  
-  const Styles = styled.div``;
-  
-  function Page () {
-    const router = useRouter();
-    const session = Session((state) => state);
-    const { user } = useAuth0();
-    const [profileUser, setProfileUser] = useState({});
-    const [profileOrganization, setProfileOrganization] = useState({});
-  
-    useEffect( () => {
-      if ( !session.user ) {
-        GetUser( user, session, setProfileUser, setProfileOrganization )
-      } else {
-        setProfileUser( session.user )
-        setProfileOrganization( session.organization )
-      }
-    } , []);
-  
-    return (
-      <Layout title="Settings">
-         <PageContainer path={ router.pathname }>
-         <Heading>
-          Settings
-        </Heading>
+  Box,
+  ButtonGroup,
+  Heading,
+  SimpleGrid,
+  Button,
+  Grid,
+  Divider,
+  Container,
+} from "@chakra-ui/react";
+import Loading from "../components/Home/Loading";
+import Layout from "../components/layout";
+import PageContainer from "../components/pageContainer";
+import styled from "@emotion/styled";
+import theme from "../public/theme";
+import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
+import Session from "../contexts/session";
+import { useRouter, withRouter } from "next/router";
+import { useEffect, useState } from "react";
 
-        { profileUser.email }
-         </PageContainer>
-      </Layout>
-    );
-  }
-  
-  export default withAuthenticationRequired( withRouter(Page), {
-    onRedirecting: () => <Loading />,
-  });
-  
+import { GetUser } from "../utils/getUser";
+
+const Styles = styled.div``;
+
+function Page() {
+  const router = useRouter();
+  const session = Session((state) => state);
+  const { user } = useAuth0();
+  const [profileUser, setProfileUser] = useState({});
+  const [profileOrganization, setProfileOrganization] = useState({});
+
+  useEffect(() => {
+    if (!session.user) {
+      GetUser(user, session, setProfileUser, setProfileOrganization);
+    } else {
+      setProfileUser(session.user);
+      setProfileOrganization(session.organization);
+    }
+  }, []);
+
+  return (
+    <Layout title="Settings">
+      <PageContainer path={router.pathname}>
+        <Heading>Settings</Heading>
+
+        {profileUser.email}
+      </PageContainer>
+    </Layout>
+  );
+}
+
+export default withAuthenticationRequired(withRouter(Page), {
+  onRedirecting: () => <Loading />,
+});
