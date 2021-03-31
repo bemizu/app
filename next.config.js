a7cad97bd21a978d8f1d67962113b49c622e627d
Sam <samullman@gmail.com>
2021-03-31 11:42:35 -0700

    halfway pwa

--- next.config.js
+++ next.config.js
@@ -1,3 +1,20 @@
+const withPWA = require('next-pwa')
+ 
+// module.exports = withPWA({
+//     pwa: {
+//         dest: 'public'
+//     },
+//     env: {
+//         AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+//         AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
+//         FAUNA_SECRET: process.env.FAUNA_SECRET, 
+//         SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
+//         SUPABASE_URL: process.env.SUPABASE_URL,
+//         SUPABASE_PUBLIC_ANON: process.env.SUPABASE_PUBLIC_ANON,
+//         AUTH0_REDIRECT: process.env.AUTH0_REDIRECT
+//     }
+// })
+
 module.exports = {
     env: {
         AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
@@ -9,3 +26,4 @@ module.exports = {
         AUTH0_REDIRECT: process.env.AUTH0_REDIRECT
     }
 }
+
--- pages/_document.js
+++ pages/_document.js
@@ -27,6 +27,13 @@ export default class MyDocument extends Document {
           `,
             }}
           />
+
+
+
+      <link rel='manifest' href='/manifest.json' />
+      <link href='icons/favicon-16x16.png' rel='icon' type='image/png' sizes='16x16' />
+      <link href='icons/favicon-32x32.png' rel='icon' type='image/png' sizes='32x32' />
+      <link rel='apple-touch-icon' href='icons/apple-icon.png'></link>
         </Head>
         <body>
 
